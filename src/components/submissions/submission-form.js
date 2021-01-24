639a8e1694e22bf3e4ee76e2d96bf17bf04d2d3c
Manuel Ott <manuel.ott@x.de>
2021-01-24 12:11:28 +0100

    Bugfixes submissions

--- src/components/submissions/submission-form.js
+++ src/components/submissions/submission-form.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './submission-form.css'
 
 export default function SubmissionForm() {
 
+    const [name, setName] = useState("")
+    const [email, setMail] = useState("")
+    const [quote, setQuote] = useState("")
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+    const handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+                "form-name": "contact",
+                "name": name,
+                "email": email,
+                "quote": quote
+            })
+        }).then(() => alert("Erfolgreich eingereicht!")).catch(error => alert(error));
+        e.preventDefault();
+    }
 
     return (
         <div className="submission">
-            <form name="contact" className="form" netlify>
+            <form onSubmit={handleSubmit} name="contact" className="form" netlify>
                 <p>
-                    <label>Your Name: <input type="text" name="name" placeholder='(Optional)' /></label>
+                    <label>Your Name: <input type="text" name="name" value={name} placeholder='(Optional)' onChange={e => {setName(e.target.value)}} /></label>
                 </p>
                 <p>
-                    <label>Your Email: <input type="email" name="email" placeholder='(Optional)' /></label>
+                    <label>Your Email: <input type="email" name="email" value={email} placeholder='(Optional)' onChange={e => {setMail(e.target.value)}} /></label>
                 </p>
                 <p>
-                    <label>Message: <textarea name="message" placeholder='"Der Boi ist schon nice."'></textarea></label>
+                    <label>Message: <textarea name="message" value={quote} placeholder='"Der Boi ist schon nice."' onChange={e => {setQuote(e.target.value)}}></textarea></label>
                 </p>
                 <button type="submit">Senden</button>
             </form>
