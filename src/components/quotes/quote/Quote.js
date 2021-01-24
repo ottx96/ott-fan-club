9cf0325e6f53872eaf44b53de8da4d23e37fe3b1
Manuel Ott <manuel.ott@x.de>
2021-01-24 20:29:20 +0100

    Design Quotes

--- src/components/quotes/quote/Quote.js
+++ src/components/quotes/quote/Quote.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from './Quote.module.css'
 
-export default function Quote( {publisher, quote} ) {
+export default function Quote({ publisher, quote }) {
 
 
     return (
         <div className={styles.quote}>
-            <h1 className={styles.text}>
-                {quote}
-            </h1>
+            <div className={styles.text}>
+                <h1>{quote}</h1>
+            </div>
+
             <h3 className={styles.publisher}>
                 - {publisher === "" && "Unbekannt"}{publisher}
             </h3>
