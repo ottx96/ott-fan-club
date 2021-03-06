07091ca7825dcfa7661668bb2ef6b51f1bdc86ee
Manuel Ott <manuel.ott@x.de>
2021-03-06 18:31:50 +0100

    Redesign navbar

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -3,14 +3,15 @@ import { Link, useHistory } from 'react-router-dom'
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
-
-
     return (
         <nav className={styles.nav}>
             <ul>
                 <li><Link to="/">Startseite</Link></li>
+                <div className={styles.separator}></div>
                 <li><Link to="/quotes">Zitate</Link></li>
+                <div className={styles.separator}></div>
                 <li><Link to="/memes">Memes</Link></li>
+                <div className={styles.separator}></div>
                 <li><Link to="/about">Über den Boi</Link></li>
             </ul>
         </nav>
