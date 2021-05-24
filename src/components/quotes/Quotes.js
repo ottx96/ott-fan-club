bddaddc04fbe812463703a3f841d080a079474b4
Ott <49874532+ottx96@users.noreply.github.com>
2021-05-24 16:22:25 +0200

    Added animations, improved Navbar (#7)
    * Added animations, improved Navbar
    
    * Improvements for mobile
    
    * Improved mobile
    
    * Improvements mobile
    
    Co-authored-by: ottx96 <manuel.ott@x.de>

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,10 +1,246 @@
 import React, { useEffect, useState } from 'react'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import styles from './Quotes.module.css'
 import Quote from './quote/Quote'
 
-export default function Quotes() {
+import Particles from 'react-tsparticles'
 
+const ParticlesParams = {
+    "autoPlay": true,
+    "background": {
+        "image": "",
+        "position": "50% 50%",
+        "repeat": "no-repeat",
+        "size": "cover",
+        "opacity": 1
+    },
+    "backgroundMask": {
+        "composite": "destination-out",
+        "cover": {
+            "color": {
+                "value": "#fff"
+            },
+            "opacity": .2
+        },
+        "enable": false
+    },
+    "fullScreen": {
+        "enable": true,
+        "zIndex": -1
+    },
+    "detectRetina": true,
+    "fpsLimit": 60,
+    "infection": {
+        "cure": false,
+        "delay": 0,
+        "enable": false,
+        "infections": 0,
+        "stages": []
+    },
+    "manualParticles": [],
+    "motion": {
+        "disable": false,
+        "reduce": {
+            "factor": 4,
+            "value": true
+        }
+    },
+    "particles": {
+        "collisions": {
+            "bounce": {
+                "horizontal": {
+                    "random": {
+                        "enable": true,
+                        "minimumValue": 0.5
+                    },
+                    "value": 1
+                },
+                "vertical": {
+                    "random": {
+                        "enable": true,
+                        "minimumValue": 0.5
+                    },
+                    "value": 2
+                }
+            },
+            "enable": true,
+            "mode": "bounce",
+            "overlap": {
+                "enable": true,
+                "retries": 5
+            }
+        },
+        "color": {
+            "value": "random",
+            "animation": {
+              "h": {
+                "count": 10,
+                "enable": true,
+                "offset": 0.1,
+                "speed": 0.25,
+                "sync": true
+              },
+              "s": {
+                "count": 10,
+                "enable": true,
+                "offset": 0.1,
+                "speed": 0.33,
+                "sync": true
+              },
+              "l": {
+                "count": 10,
+                "enable": true,
+                "offset": 0.1,
+                "speed": 0.5,
+                "sync": true
+              }
+            }
+          },
+        "destroy": {
+            "mode": "none",
+            "split": {
+                "count": 1,
+                "factor": {
+                    "random": {
+                        "enable": true,
+                        "minimumValue": 0
+                    },
+                    "value": 3
+                },
+                "rate": {
+                    "random": {
+                        "enable": true,
+                        "minimumValue": 0
+                    },
+                    "value": {
+                        "min": 4,
+                        "max": 15
+                    }
+                }
+            }
+        },
+        "life": {
+            "count": 0,
+            "delay": {
+                "random": {
+                    "enable": true,
+                    "minimumValue": 0
+                },
+                "value": 0,
+                "sync": false
+            },
+        },
+        "links": {
+            "blink": true,
+            "color": {
+                // "value": "#E76F51"
+                "value": "#bbbbbb"
+            },
+            "consent": false,
+            "distance": 375,
+            "enable": false,
+            "frequency": .25,
+            "opacity": 1,
+            "triangles": {
+                "enable": true,
+                "frequency": .33,
+                "color": {
+                    "value": "#F4A261",
+                },
+            },
+            "width": 2,
+            "warp": false
+        },
+        "move": {
+            "angle": {
+                "offset": 45,
+                "value": 90
+            },
+            "attract": {
+                "enable": true,
+                "rotate": {
+                    "x": 600,
+                    "y": 1200
+                }
+            },
+            "decay": 0,
+            "distance": 0,
+            "direction": "none",
+            "drift": 0,
+            "enable": true,
+            "outModes": {
+                "default": "bounce",
+                // "bottom": "out",
+                // "left": "out",
+                // "right": "out",
+                // "top": "out"
+            },
+            "random": true,
+            "size": true,
+            "speed": 1,
+            "straight": true,
+            "vibrate": false,
+            "warp": true
+        },
+        "number": {
+            "density": {
+                "enable": false,
+                "area": 1000,
+                "factor": 2000
+            },
+            "limit": 50,
+            "value": 3
+        },
+        "opacity": {
+            "random": {
+                "enable": true,
+                "minimumValue": 0.2,
+                "maximumValue": 0.75
+            },
+            "animation": {
+                "count": 1,
+                "enable": false,
+                "speed": 0.2,
+                "sync": true,
+                "destroy": "none",
+                "minimumValue": 0.6,
+                "startValue": "random"
+            }
+        },
+        "reduceDuplicates": true,
+        "shape": {
+            "options": {},
+            "type": "circle"
+        },
+        "size": {
+            "random": {
+                "enable": true,
+                "minimumValue": 1
+            },
+            "value": {
+                "min": 1,
+                "max": 115
+            },
+            "animation": {
+                "count": 0,
+                "enable": true,
+                "speed": 2,
+                "sync": true,
+                "destroy": "none",
+                "minimumValue": 0,
+                "startValue": "random"
+            }
+        },
+    },
+    "pauseOnBlur": true,
+    "pauseOnOutsideViewport": true,
+    "responsive": [],
+    "themes": []
+}
+
+
+
+export default function Quotes() {
     const SITE_ID = "d0798c62-bf24-49c9-a7ee-6fe52d403f7c"
     const FORM_ID = "600d5835182c38000865997c"
 
@@ -35,20 +271,20 @@ export default function Quotes() {
     let history = useHistory()
 
     const pushQuotes = e => {
-        history.push('/submit/quote') 
+        history.push('/submit/quote')
     }
 
     return (
-        <div className={styles.quotes}>
-            <div className={styles.circle_1}></div>
-            <div className={styles.circle_2}></div>
-            <div className={styles.circle_3}></div>
-            <button className={styles.add} onClick={pushQuotes}>
-                +
+        <div className={styles.page}>
+            <div className={styles.quotes}>
+                <button className={styles.add} onClick={pushQuotes}>
+                    +
             </button>
-            {quotes.map(q => (
-                <Quote key={q.data.quote} publisher={q.name} quote={q.data.quote} />
-            ))}
+                {quotes.map(q => (
+                    <Quote key={q.data.quote} publisher={q.name} quote={q.data.quote} />
+                ))}
+            </div>
+            <Particles classname={styles.particles} width="100%" height="90vh" id="tsparticles" options={ParticlesParams} />
         </div>
     )
 }
