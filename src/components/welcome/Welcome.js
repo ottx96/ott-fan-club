33c20a3566269550d4a846d57e3e4d6706e91cde
ottx96 <manuel.ott@x.de>
2021-07-04 15:20:02 +0200

    Added nyan cat sectioni

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Welcome.module.css'
-import HollowKnightSection from './sections/hollowknight/HollowKnight'
+import NyanCatSection from './sections/hollowknight/HollowKnight'
 import { Container, Jumbotron, Row, Col } from 'react-bootstrap'
 
 export default function Welcome() {
@@ -8,7 +8,7 @@ export default function Welcome() {
         <Container className={styles.welcome}>
             <Row classname={styles.section}>
                 <Col>
-                    <HollowKnightSection />
+                    <NyanCatSection />
                 </Col>
             </Row>
         </Container>
