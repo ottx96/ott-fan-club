3939266fc2cb4535dc5b958928bb4c4c11733231
Ott <49874532+ottx96@users.noreply.github.com>
2021-09-12 22:29:53 +0200

    Redesigning landing page (#11)
    * redesigned landinig page
    
    * changed background color
    
    * improved screen sizes
    
    * Changed particle frequency
    
    Co-authored-by: Manuel Ott <manuel.ott@mait.de>

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import styles from './Welcome.module.css'
 import NyanCatSection from './sections/nyancat/NyanCat'
-import { Container, Jumbotron, Row, Col } from 'react-bootstrap'
+import LandingSection from './sections/landing/Landing'
 
 export default function Welcome() {
     return (
         <div className={styles.container}>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <NyanCatSection />
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
-            <section className={styles.snap}/>
+            <LandingSection />
+            <div className={styles.glass}>
+                <h1>Manuel Ott</h1>
+                <h2>Technical Developer</h2>
+                <div className={styles.social}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="" />
+                    <img src="https://allfacebook.de/wp-content/uploads/2020/02/ig-logo-bw.png" alt="" />
+                    <img src="https://image.flaticon.com/icons/png/512/49/49656.png" alt="" />
+                    <img src="https://image.flaticon.com/icons/png/512/48/48910.png" alt="" />
+                </div>
+            </div>
+            
+            
+            {/* <section className={styles.snap}/> */}
+            {/* <section className={styles.snap}/> */}
+            {/* <NyanCatSection /> */}
         </div>
     )
 
